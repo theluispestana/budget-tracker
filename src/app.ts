aeab636526ea0590599eb6ad33e500f7a7fee2fa
theluispestana <theluispestana@gmail.com>
2020-10-05 16:39:57 -0400

    added required options and corrected atlas auth

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,12 @@ import mongoose from "mongoose";
 const app = express();
 const PORT = process.env.port || 3000;
 
-mongoose.connect(
-  "mongodb+srv://budget-tracker-api:" +
-    process.env.MONGO_ATLAS_PW +
-    "@cluster0.12gli.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  {
-    useMongoClient: true,
-  }
-);
+const uri = `mongodb+srv://budget-tracker-api:${process.env.MONGO_ATLAS_PW}@cluster0.12gli.mongodb.net/budget_tracker_api?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World test");
