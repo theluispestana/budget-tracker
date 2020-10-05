bcb97d088fa49982cffbe034067b9e06b73efaae
theluispestana <theluispestana@gmail.com>
2020-10-05 16:17:40 -0400

    connected to mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 const PORT = process.env.port || 3000;
+
+mongoose.connect(
+  "mongodb+srv://budget-tracker-api:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0.12gli.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  {
+    useMongoClient: true,
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World test");
