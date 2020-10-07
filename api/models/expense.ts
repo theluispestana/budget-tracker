2818c6f80cd9d6ea77d7def49debef0e9f9ada6b
theluispestana <theluispestana@gmail.com>
2020-10-07 14:17:49 -0400

    corrected error in expense model

--- api/models/expense.ts
+++ api/models/expense.ts
@@ -2,14 +2,14 @@ import mongoose, { Document, Schema } from "mongoose";
 import { IUser } from "./user";
 
 export interface IExpense extends Document {
-  owner: IUser["_id"];
-  name: String;
-  amount: Number;
-  frequency: String;
+  owner: IUser;
+  name: string;
+  amount: number;
+  frequency: string;
 }
 
 const ExpenseSchema: Schema = new Schema({
-  owner: { type: Schema.Types.ObjectId, required: true },
+  owner: { type: Object, required: true },
   name: { type: String, required: true },
   amount: { type: Number, required: true },
   frequency: { type: Number, required: true },
